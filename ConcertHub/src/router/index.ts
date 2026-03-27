515a7699c0f281770035de5ee247dd376899035e
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-03-27 23:11:24 +0100

    Routes setup

--- ConcertHub/src/router/index.ts
+++ ConcertHub/src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import ConcertView from '../views/ConcertListView.vue'
+import MyListView from '../views/MyListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +11,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: DashboardView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/concerts',
+      name: 'concerts',
+      component: ConcertView,
+    },
+    {
+      path: '/mylist',
+      name: 'mylist',
+      component: MyListView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
   ],
 })
