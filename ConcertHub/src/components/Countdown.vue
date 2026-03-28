674dbe43fca179dcad7d3d8c721fda4924fe211d
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-03-28 22:56:53 +0100

    Countdown

--- ConcertHub/src/components/Countdown.vue
+++ ConcertHub/src/components/Countdown.vue
@@ -2,5 +2,27 @@
 <template>
   <div class="p-3 containerHeight containerBorder">
     <p class="subtitle">Countdown</p>
+    <div class="d-flex gapContent align-items-center justify-content-center">
+      <div class="countdownNumber">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <span class="countdownNumberFontSize">120</span>
+          <span class="bold">D</span>
+        </div>
+      </div>
+      <span class="countdownNumberDelimiter">:</span>
+      <div class="countdownNumber">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <span class="countdownNumberFontSize">10</span>
+          <span class="bold">H</span>
+        </div>
+      </div>
+      <span class="countdownNumberDelimiter">:</span>
+      <div class="countdownNumber">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <span class="countdownNumberFontSize">14</span>
+          <span class="bold">M</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
