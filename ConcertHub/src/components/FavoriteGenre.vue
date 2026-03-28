0eb1a740f063f79920f932f62663ff35df9b96d7
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-03-28 22:14:16 +0100

    Favorite genre

--- ConcertHub/src/components/FavoriteGenre.vue
+++ ConcertHub/src/components/FavoriteGenre.vue
@@ -2,5 +2,10 @@
 <template>
   <div class="p-3 containerHeight containerBorder">
     <p class="subtitle">Favorite genre</p>
+    <ol class="favoriteGenreList">
+      <li>Rock</li>
+      <li>Pop</li>
+      <li>Metal</li>
+    </ol>
   </div>
 </template>
