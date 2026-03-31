edd50d8ea0639fe800851fd22938f1a0d6abbf93
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-03-31 21:11:58 +0200

    Search concert logic

--- ConcertHub/src/components/ConcertListComponent.vue
+++ ConcertHub/src/components/ConcertListComponent.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import ConcertListCard from './ConcertListCard.vue'
 import axios from 'axios'
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, computed, onMounted, onUnmounted, watch } from 'vue'
 
 const emit = defineEmits(['get-concert-id'])
 
-const concerts = ref(null)
-const selectedConcertId = ref('')
-const isLoading = ref(false)
+let concerts = ref<any[]>([])
+const selectedConcertId = ref<string>('')
+const isLoading = ref<boolean>(false)
+let model = defineModel<string>({ default: '' })
+const concertCardSearch = computed(() => {
+  const search = model.value?.toLowerCase() || ''
+
+  if (!search) return concerts.value
+
+  return concerts.value.filter((el: any) =>
+    el.name.toLowerCase().includes(search)
+  )
+})
 
 let interval: ReturnType<typeof setInterval> | undefined = undefined
 
@@ -21,12 +31,12 @@ const handleGetConcertID = (concertId: string) => {
 const fetchConcerts = async () => {
   try {
     const res = await axios.get('http://localhost:3000/api/get-concert-list')
-    concerts.value = res.data
-    isLoading.value = false
+    concerts.value = res.data || []
   } catch (error) {
     console.error('Server down ili error')
-    concerts.value = null
-    isLoading.value = true
+    concerts.value = []
+  } finally {
+    isLoading.value = false
   }
 }
 
@@ -51,11 +61,11 @@ onUnmounted(() => {
       <div class="row gx-3 gy-2 mb-2 mb-md-0">
         <div class="d-flex gap-2 justify-content-start align-items-center">
           <i class="bi bi-search"></i>
-          <input type="text" placeholder="Search" class="searchInput" />
+          <input type="text" placeholder="Search" v-model="model" class="searchInput" />
         </div>
         <div class="d-flex flex-wrap gap-4 justify-content-evenly">
           <ConcertListCard
-            v-for="concert in concerts"
+            v-for="concert in concertCardSearch"
             :key="concert.id"
             :data="concert"
             @get-concert-id="handleGetConcertID"
