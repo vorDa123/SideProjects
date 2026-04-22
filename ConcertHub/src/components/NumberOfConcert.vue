f0db8a483e5be59c3fc64f41a61cf2de16e371be
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-04-22 17:01:45 +0200

    Favorite concert number animation

--- ConcertHub/src/components/NumberOfConcert.vue
+++ ConcertHub/src/components/NumberOfConcert.vue
@@ -1,35 +1,76 @@
 <script setup lang="ts">
-import { motion } from 'motion-v'
-import { computed, onMounted, onUnmounted } from 'vue'
+import { ref, computed, onMounted, watch, nextTick } from 'vue'
 import { useHandleConcertStore } from '../stores/ConcertsStore.ts'
+import { gsap } from 'gsap'
+
+const numberAnimation = ref<HTMLElement | null>(null)
 
 const handleFavoriteStore = useHandleConcertStore()
 
-let interval: ReturnType<typeof setInterval> | undefined = undefined
+let numberOfConcerts = computed(() => handleFavoriteStore.favorites.length)
 
 const fetchFavourites = async () => {
   await handleFavoriteStore.getFavorites()
 }
 
-let numberOfConcerts = computed(() => handleFavoriteStore.favorites.length)
+watch(
+  numberOfConcerts,
+  async (newVal: number) => {
+    await nextTick(() => {
+      if (!numberAnimation.value) return
+      const oldElement = numberAnimation.value.querySelector('.number')
+      if (!oldElement) return
+
+      gsap.set(oldElement, { position: 'absolute', top: 0 })
+
+      const newElement = document.createElement('span')
+      newElement.className = 'number'
+      newElement.innerText = newVal.toString()
+
+      gsap.set(newElement, {
+        position: 'absolute',
+        top: 0,
+        opacity: 0,
+      })
+
+      numberAnimation.value.appendChild(newElement)
+
+      const tl = gsap.timeline()
+
+      tl.to(oldElement, {
+        y: -20,
+        opacity: 0,
+        duration: 0.3,
+        ease: 'power2.in',
+      }).to(
+        newElement,
+        {
+          y: 0,
+          opacity: 1,
+          duration: 0.8,
+          ease: 'elastic.out(1.1, 0.4)',
+          onComplete: () => {
+            oldElement.remove()
+          },
+        },
+        '-=0.2',
+      )
+    })
+  },
+  { immediate: true },
+)
 
 onMounted(() => {
   fetchFavourites()
-
-  interval = setInterval(() => {
-    fetchFavourites()
-  }, 1000)
-})
-
-onUnmounted(() => {
-  clearInterval(interval ?? undefined)
 })
 </script>
 <template>
   <div
     class="p-3 d-flex flex-column align-items-center justify-content-center containerHeight containerBorder"
   >
-    <span class="bold concertNumber">{{ numberOfConcerts }}</span>
+    <div ref="numberAnimation" class="numberDisplayWrapper concertNumber bold">
+      <span class="number">0</span>
+    </div>
     <span>Concerts</span>
   </div>
 </template>
