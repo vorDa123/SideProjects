cdb65bba769b2359e51e1d94edac1f35cacf582a
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-08-04 16:28:11 +0200

    bowling centers scroll for multiple items

--- BowlingBuddiesFrontend/src/components/BowlingCenterCard.tsx
+++ BowlingBuddiesFrontend/src/components/BowlingCenterCard.tsx
@@ -3,7 +3,7 @@ type BowlingCenterCardProps = {
 };
 function BowlingCenterCard({ bowlingCenterName }: BowlingCenterCardProps) {
   return (
-    <div className="w-43.75 h-43.75 shadow-mob rounded-m20 bg-[url(/src/assets/playerBowling.jpg)] bg-center bg-cover">
+    <div className="snap-start shrink-0 w-43.75 h-43.75 shadow-mob rounded-m20 bg-[url(/src/assets/playerBowling.jpg)] bg-center bg-cover">
       <div className="w-full h-full rounded-m20 bg-linear-to-t from-black-70 to-black-00 flex flex-col justify-end items-center">
         <p className="text-white-100 text-mh2 pb-2 text-center max-w-full font-medium">
           {bowlingCenterName}
--- BowlingBuddiesFrontend/src/components/BowlingCentersDashboard.tsx
+++ BowlingBuddiesFrontend/src/components/BowlingCentersDashboard.tsx
@@ -4,8 +4,9 @@ function BowlingCentersDashboard() {
   return (
     <section className="pt-5 pb-2.5">
       <h1 className="text-mh1 font-semibold">Bowling Center</h1>
-      <div className="flex flex-row gap-3.5 pt-2.5">
+      <div className="flex flex-row flex-nowrap gap-2 snap-x pt-2.5 overflow-x-scroll">
         <BowlingCenterCard bowlingCenterName="West Bowling"/>
+        <BowlingCenterCard bowlingCenterName="Kooglana"/>
         <BowlingCenterCard bowlingCenterName="Kooglana"/>
       </div>
     </section>
