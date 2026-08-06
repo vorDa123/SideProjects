0ea5d61e85a2cd6d605aceb257aebddba8677130
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-08-06 16:48:36 +0200

    Open join bowling page responsive finished

--- BowlingBuddiesFrontend/src/components/OpenJoinDashboard.tsx
+++ BowlingBuddiesFrontend/src/components/OpenJoinDashboard.tsx
@@ -1,16 +1,36 @@
 import OpenJoinCard from "./OpenJoinCard.tsx";
 
-function OpenJoinDashboard() {
+type OpenJoinProps = {
+  openJoinBowlingPage?: boolean;
+};
+
+function OpenJoinDashboard({ openJoinBowlingPage = false }: OpenJoinProps) {
   return (
-    <section className="pt-2.5 pb-2.5 w-full lg:col-span-6 xl:col-span-4 xxl:col-span-6">
-      <h1 className="text-mh1 font-semibold">Open Join</h1>
-      <div className="grid grid-cols-1 gap-3 pt-2">
-        <OpenJoinCard />
-        <OpenJoinCard />
-        <OpenJoinCard />
-        <p className="text-mlinks text-right max-w-94.25 md:text-tlinks">Show more {'>'}</p>
-      </div>
-    </section>
+    <>
+      {openJoinBowlingPage ? (
+        <section className="pt-2.5 pb-2.5 w-full md:col-span-2 lg:col-span-12 xxl:col-span-24">
+          <h1 className="text-mh1 font-semibold">Open Join</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xxl:grid-cols-4 gap-3 pt-2">
+            <OpenJoinCard />
+            <OpenJoinCard />
+            <OpenJoinCard />
+            <OpenJoinCard />
+          </div>
+        </section>
+      ) : (
+        <section className="pt-2.5 pb-2.5 w-full lg:col-span-6 xl:col-span-4 xxl:col-span-6">
+          <h1 className="text-mh1 font-semibold">Open Join</h1>
+          <div className="grid grid-cols-1 gap-3 pt-2">
+            <OpenJoinCard />
+            <OpenJoinCard />
+            <OpenJoinCard />
+            <p className="text-mlinks text-right max-w-94.25 md:text-tlinks">
+              Show more {">"}
+            </p>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
