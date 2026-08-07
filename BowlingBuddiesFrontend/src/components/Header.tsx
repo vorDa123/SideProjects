3f358c4494970943f7dd61f8b750f863097d2299
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-08-07 19:29:58 +0200

    Profile page mobile finished

--- BowlingBuddiesFrontend/src/components/Header.tsx
+++ BowlingBuddiesFrontend/src/components/Header.tsx
@@ -1,14 +1,20 @@
 import Logo from "../assets/BBLogo.svg";
 
-import type {HeaderProps} from "../types/Types.ts"
+import type { HeaderProps } from "../types/Types.ts";
 
 function HeaderMob({ showLogo = true }: HeaderProps) {
   return (
     <header className="flex flex-row gap-2 bg-lighterBlue-100 items-center justify-center rounded-bl-4xl rounded-br-4xl h-[21.4vh] lg:hidden">
-      {showLogo ? <img src={Logo} width={48} height={76} /> : <p></p>}
-      <p className="text-white-100 font-medium text-3xl w-30">
-        Bowling Buddies
-      </p>
+      {showLogo ? (
+        <>
+          <img src={Logo} width={48} height={76} />
+          <p className="text-white-100 font-medium text-3xl w-30">
+            Bowling Buddies
+          </p>
+        </>
+      ) : (
+        <p></p>
+      )}
     </header>
   );
 }
--- BowlingBuddiesFrontend/src/views/Profile.tsx
+++ BowlingBuddiesFrontend/src/views/Profile.tsx
@@ -1,17 +1,61 @@
 import HeaderMob from "../components/Header.tsx";
 import Navigation from "../components/Navigation.tsx";
 import UserImg from "../assets/playerBowling.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router";
+
+import {
+  faUser,
+  faBell,
+  faCircleInfo,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Profile() {
   return (
     <>
-      <div className="grid grid-cols-1">
+      <div className="grid grid-cols-1 relative">
         <HeaderMob showLogo={false} />
         <img
           src={UserImg}
           alt="user image"
-          className="rounded-full border-6 border-white w-50 h-50 absolute left-1/2 top-1/14 transform -translate-x-1/2 -translate-y-1/14"
+          className="rounded-full border-6 border-white w-50 h-50 absolute left-1/2 top-1/14 transform -translate-x-1/2"
         />
+        <p className="text-mh3 text-center mt-20">Davor Kuharić</p>
+        <div className="flex flex-col items-center justify-around gap-10 mt-12">
+          <NavLink to="/myprofile">
+            <div className="bg-lighterBlue-100 h-15.5 w-70.5 rounded-m60 relative">
+              <div className="flex flex-row gap-5 text-mh2 text-white-100 absolute left-1/6 top-1/3">
+                <FontAwesomeIcon icon={faUser} />
+                <p>My Profile</p>
+              </div>
+            </div>
+          </NavLink>
+          <NavLink to="/">
+            <div className="bg-lighterBlue-100 h-15.5 w-70.5 rounded-m60 relative">
+              <div className="flex flex-row gap-5 text-mh2 text-white-100 absolute left-1/6 top-1/3">
+                <FontAwesomeIcon icon={faBell} />
+                <p>Notifications</p>
+              </div>
+            </div>
+          </NavLink>
+          <NavLink to="/">
+            <div className="bg-lighterBlue-100 h-15.5 w-70.5 rounded-m60 relative">
+              <div className="flex flex-row gap-5 text-mh2 text-white-100 absolute left-1/6 top-1/3">
+                <FontAwesomeIcon icon={faCircleInfo} />
+                <p>About Us</p>
+              </div>
+            </div>
+          </NavLink>
+          <NavLink to="/login">
+            <div className="bg-lighterBlue-100 h-15.5 w-70.5 rounded-m60 relative">
+              <div className="flex flex-row gap-5 text-mh2 text-white-100 absolute left-1/6 top-1/3">
+                <FontAwesomeIcon icon={faRightFromBracket} />
+                <p>Log out</p>
+              </div>
+            </div>
+          </NavLink>
+        </div>
         <div className="lg:hidden">
           <Navigation />
         </div>
