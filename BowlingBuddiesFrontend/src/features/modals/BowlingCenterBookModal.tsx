eee543a3f1ad431a8ca1f52018e88c14958de3bd
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-08-27 01:34:07 +0200

    Refactor BowlingCenterBookingModal and EditBookingModal to use dynamic height based on join state; improve modal styling and transition effects

--- BowlingBuddiesFrontend/src/features/modals/BowlingCenterBookModal.tsx
+++ BowlingBuddiesFrontend/src/features/modals/BowlingCenterBookModal.tsx
@@ -9,6 +9,7 @@ import { BookingFormContext } from "../../context/BookFormContext.ts";
 
 function BowlingCenterBookingModal(props: ModalProps) {
   const [isJoinClicked, setIsJoinClicked] = useState<boolean>(false);
+  const currentHeight = isJoinClicked ? "95dvh" : "80dvh";
   const handleCloseModal = (e: React.MouseEvent<Element>) => {
     e.stopPropagation();
     if (!props.onClose) return;
@@ -39,7 +40,10 @@ function BowlingCenterBookingModal(props: ModalProps) {
         className="z-50 bg-darkerBlue-60 h-dvh w-dvw fixed inset-0 touch-none"
         onClick={handleCloseModal}
       ></div>
-      <div className="bg-white-100 mx-auto my-5 px-6 py-7 w-9/10 h-[95dvh] fixed bottom-0 left-0 right-0 z-60 rounded-m30 md:w-8/10 lg:w-6/10 xl:w-5/10 xxl:w-4/10 md:py-5 xl:py-5 mxl:py-10 xxl:py-5">
+      <div
+        style={{ height: currentHeight }}
+        className="bg-white-100 mx-auto my-5 px-6 py-7 w-9/10 fixed top-0 left-0 right-0 z-60 rounded-m30 md:w-8/10 lg:w-6/10 xl:w-5/10 xxl:w-4/10 md:py-5 xl:py-5 mxl:py-10 xxl:py-5 transition-all duration-300"
+      >
         <div className="overflow-y-auto h-full">
           <div className="grid grid-cols-8 gap-x-4 auto-rows-max">
             <BookModalHeader onClose={closeModal} />
