0d8a62c4dc9ab253e2004ee6e92002c0d2ea9362
Davor Kuharić <62572560+vorDa123@users.noreply.github.com>
2026-09-12 19:01:59 +0200

    fix: adjust loading skeleton layout for better responsiveness

--- BowlingBuddiesFrontend/src/features/bowling-centers/BowlingCentersSection.tsx
+++ BowlingBuddiesFrontend/src/features/bowling-centers/BowlingCentersSection.tsx
@@ -11,9 +11,9 @@ function BowlingCentersSection() {
 
   return isLoading ? (
     <section className="pt-5 pb-2.5 w-full lg:col-span-12 md:col-span-2 xxl:col-span-24">
-      <div className="flex flex-row items-center justify-between">
-        <div className="bg-gray-400 w-100 h-10 rounded-m20"></div>
-        <div className="bg-gray-400 w-150 h-10 rounded-m20"></div>
+      <div className="flex flex-row items-center justify-between gap-10">
+        <div className="bg-gray-400 w-2/3 md:w-1/2 lg:w-3/5 h-7 rounded-m20"></div>
+        <div className="bg-gray-400 w-1/3 lg:w-2/5 md:w-1/2 h-7 rounded-m20"></div>
       </div>
       <div className="flex flex-row flex-nowrap gap-2 snap-x py-2.5 overflow-x-auto md:gap-3.75 xl:gap-5">
         <div className="bg-gray-400 shrink-0 w-45.25 h-48.25 rounded-m20"></div>
